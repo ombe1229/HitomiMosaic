5cfe289fd518d58728cfc548565f2bdf96896fa6
dp0973 <yeong0973@gmail.com>
2021-10-18 18:12:01 +0900

    refactor(index): simplify code

--- index.js
+++ index.js
@@ -22,16 +22,10 @@ setTimeout(function () {
     const galleryContent = document.querySelector(".gallery-content");
     const containerList = galleryContent.querySelectorAll(".manga, .acg, .dj");
     containerList.forEach(function (element) {
-        let isDisgusting = false;
         let tagContainer = element.querySelector(".relatedtags");
-        let tags = tagContainer.querySelectorAll("li");
-        for (let tag of tags) {
-            if (tagList.includes(tag.innerText)) {
-                isDisgusting = true;
-                break;
-            };
-        };
-        if (isDisgusting) {
+        let tags = tagContainer.querySelectorAll("li").map(tag => tag.innerText);
+
+        if (tags.some(tag => tagList.includes(tag))) {
             element.querySelector(".dj-img-cont").remove();
         }
     });
